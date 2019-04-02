87c9b2ed08068df7b869db6ffa1fc9dd6a025989
Michael Hesselberg <mihessel@microsoft.com>
2019-04-02 12:04:45 +0200

    added Address column

--- WebApp.Database/dbo/Tables/Employee.sql
+++ WebApp.Database/dbo/Tables/Employee.sql
@@ -4,6 +4,7 @@
     [Department] NVARCHAR (MAX) NULL,
     [Email] NVARCHAR(50) NULL, 
 	[Phone] NVARCHAR(50) NULL, 
+	[Address] NVARCHAR(50) NULL, 
     CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
